b879b6fe4c317efeb6c3861518735d2235536d09
Aditya Mahaddalkar <adityam1311@gmail.com>
2018-06-30 10:03:21 +0530

    Removed validating ip

--- A3/findSubnet.py
+++ A3/findSubnet.py
@@ -1,52 +1,14 @@
 ''' Accepts an ip, verifies whether it
 is valid and finds the subnet
 '''
-ip = '0.0.0.0'
+import ipaddress
 classIp = 'A'
-subnets = {'A': '255.0.0.0', 'B': '255.255.0.0',
-           'C': '255.255.255.0', 'D': '255.255.255.255', 'E': 'Invalid'}
-
-
-def validateIP(ip='0.0.0.0', classIP='A'):
-    ''' Validates ip range as well as class'''
-    octets = list(map(int, ip.split('.')))
-    for number in octets:
-        if not number in range(0, 256):
-            return False
-
-    if not octets[0] in range(0, 128) and classIP == 'A':
-        return False
-
-    if not octets[0] in range(128, 192) and classIP == 'B':
-        return False
-
-    if not octets[0] in range(192, 224) and classIP == 'C':
-        return False
-
-    if not octets[0] in range(224, 240) and classIP == 'D':
-        return False
-
-    if not octets[0] in range(240, 256) and classIP == 'E':
-        return False
-
-    return True
 
 
 def inputIp():
     ''' Inputs the ip'''
-    global ip
-    ip = str(input('Enter the ip: '))
-    global classIp
-    classIp = findClass(ip)
-    if classIp == 'Invalid':
-        print('Invalid ip address')
-        return
-    if not validateIP(ip, classIp):
-        print('Invalid ip address')
-        return
-    else:
-        print('Valid ip')
-    return
+    ip = str(input('Enter ip address'))
+    return ipaddress.IPv4Address(ip)
 
 
 def findClass(ip='0.0.0.0'):
