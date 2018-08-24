b3c39b18474180df606543651faf461774518d99
AdityaMahaddalkar <adityam1311@gmail.com>
2018-08-23 18:17:58 -0700

    Initialized for simple chat

---
+++ CPP_UDP_File_Transfer/Client.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define PORT 8099
+
+
+int main(){
+	int sockfd;
+	struct sockaddr_in serverAddr;
+	char buffer[1024];
+	socklen_t addr_size;
+
+	sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	strcpy(buffer, "Hello server\n");
+	sendto(sockfd, buffer, 1024, 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	printf("[+]Data sent: %s", buffer);
+}
