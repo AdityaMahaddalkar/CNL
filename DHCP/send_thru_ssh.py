9762de8d79b5d650ff0a2dcdff4700b027f7fbd2
MPA <sam@email.com>
2018-08-24 08:02:20 +0530

    Finalized commit

--- DHCP/send_thru_ssh.py
+++ DHCP/send_thru_ssh.py
@@ -4,8 +4,8 @@ try:
 	print('Entering arp command')
 	os.system('arp')
 	ip_add = input('Enter the ip to which you want to install: ')
-	os.system('scp installer.py root@' + ip + '/home/tecomp')
-	os.system("sshpass -p 'tecomp' root@" + ip)
+	os.system('scp installer.py root@' + ip_add + ':/home/tecomp')
+	os.system("ssh root@" + ip_add)
 	print('Success')
 except Exception as e:
 	print(e)
