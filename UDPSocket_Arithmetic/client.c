bb14dcce5d50f7c83760dab139a5dade3c4ae6c5
AdityaMahaddalkar <adityam1311@gmail.com>
2018-08-29 14:55:10 +0530

    Add files via upload

---
+++ UDPSocket_Arithmetic/client.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#define PORT 8099
+void main(int argc, char const *argv[])
+{
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        
+    }
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+       
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        
+    }
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    //valread = read( sock , buffer, 1024);
+    //printf("%s\n",buffer );
+    
+    // File transfer
+    char buffer2[10];
+    FILE *fp;
+    fp = fopen("file", "r");
+    fscanf(fp, "%s", &buffer2);
+    send(sock, buffer2, strlen(buffer2), 0);
+    fclose(fp);
+    
+    // Arithmetic 
+    
+    printf("\n1. to add\n2. to sub\n3.to mul\n4.to div:");
+    char option[1];
+    scanf("%s", &option);
+    send(sock, option, strlen(option), 0);
+    
+    printf("Enter num1: ");
+    char num1[1];
+    scanf("%s", &num1);
+    send(sock, num1, strlen(num1), 0);
+    
+    printf("Enter num2: ");
+    char num2[1];
+    scanf("%s", &num2);
+    send(sock, num2, strlen(num2), 0);
+     
+}
---
+++ UDPSocket_Arithmetic/server.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#define PORT 8099
+void main(){
+
+	int server_fd, new_socket, valread;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	char buffer[1024] = {0};
+	string message
+	printf("Enter message: ");
+	scanf("%s", &message);
+	strcpy(buffer, message);
+
+	if( (server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		perror("socket failed");
+        exit(EXIT_FAILURE);
+	}
+
+	if( setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+	{
+		perror("setsockopt");
+        exit(EXIT_FAILURE);
+	}
+	memset(&address, 0, addrlen);
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = SOCK_STREAM;
+	address.sin_port = htons(PORT);
+
+	if (bind(server_fd, (struct sockaddr *)&address, 
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+                       (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read(new_socket, buffer, 1024);
+    printf('%s\n', buffer);
+}
